dd5e6242c22e7db9c13e5ebd4b2e72a707530899
Parag Pachpute <paragpachpute3@gmail.com>
2016-01-27 19:32:42 +0530

    first commit

--- app/src/main/java/com/paragdev/myportfolioapp/MainActivity.java
+++ app/src/main/java/com/paragdev/myportfolioapp/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
